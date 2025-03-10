0dca3fd298fc67a9763ca3dd69fff5178bf18b3e
unknown <smariesabdou@gmail.com>
2025-03-10 23:36:56 +0000

    Creation du projet et des classes vers la bd code first

---
+++ Système de Gestion des Étudiants/Système de Gestion des Étudiants/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Système_de_Gestion_des_Étudiants.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Système_de_Gestion_des_Étudiants.DBGestionContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Système_de_Gestion_des_Étudiants.DBGestionContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
